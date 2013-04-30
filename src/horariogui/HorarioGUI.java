7886f66aa4b67941a859de4762a4b7161d863bc9
Nelson Rafael <1101369@isep.ipp.pt>
2013-04-30 11:22:49 +0100

    F1RST

---
+++ src/horariogui/HorarioGUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horariogui;
+
+/**
+ *
+ * @author Guna
+ */
+public class HorarioGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
